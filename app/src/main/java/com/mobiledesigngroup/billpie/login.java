6c6d39bf8cb2aa673033fe92f61896770c9ac284
xeeraki <shafai86@gmail.com>
2017-11-13 13:40:11 +0100

    new activity added

--- app/src/main/java/com/mobiledesigngroup/billpie/login.java
+++ app/src/main/java/com/mobiledesigngroup/billpie/login.java
@@ -1,10 +1,33 @@
 package com.mobiledesigngroup.billpie;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
 /**
  * Created by adam on 2017-11-11.
  */
+public class login extends AppCompatActivity{
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+        Button btnLogin = findViewById(R.id.button);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+@Override
+public void onClick(View view) {
 
-public class login {
+    Intent intent = new Intent(getBaseContext(), MainActivity.class);
+    startActivity(intent);
+
+        }
+        });
+
+
+        }
+
 
 
 
