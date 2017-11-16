3ec2498c9359f364415fbb143a44eb6f0239d42f
Kinsark <eric.renmail@gmail.com>
2017-11-16 11:42:00 +0100

    Added dashboard and event-page

---
+++ webApp/src/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-dashboard',
+  templateUrl: 'dashboard.component.html'
+})
+export class DashboardComponent {
+  constructor(public navCtrl: NavController) {}
+
+}
