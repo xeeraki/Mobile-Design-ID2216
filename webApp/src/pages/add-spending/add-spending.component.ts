a84b3e9f1659e0ce8a8f18fbb708f11c175240ac
Cassius Garat <cassius.garat@phelma.grenoble-inp.fr>
2017-11-16 12:33:18 +0100

    add a spending

---
+++ webApp/src/pages/add-spending/add-spending.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'add-spending',
+  templateUrl: 'add-spending.component.html'
+})
+export class AddSpending {
+
+  constructor(public navCtrl: NavController) {}
+
+}
