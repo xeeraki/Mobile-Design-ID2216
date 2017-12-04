a19c57fbdb1eca3f4a18c7c4b290a8d20d2d9fbb
xeeraki <shafai86@gmail.com>
2017-12-04 18:54:10 +0100

    fab button only visible on Events and linked to CreateEvent

--- androidApp/BillPie3/app/src/main/java/com/mobiledesigngroup/billpie3/Events.java
+++ androidApp/BillPie3/app/src/main/java/com/mobiledesigngroup/billpie3/Events.java
@@ -1,8 +1,10 @@
 package com.mobiledesigngroup.billpie3;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -25,6 +27,15 @@ public class Events extends Fragment{
         View view = inflater.inflate(R.layout.events, container, false);
         ListView listView = (ListView) view.findViewById(R.id.list_view);
 
+        FloatingActionButton fab = view.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), CreateEvent.class);
+                startActivity(intent);
+            }
+        });
+
         mDatabase = new BillBaseHelper(getActivity());
         ArrayList<String> list = new ArrayList<>();
         Cursor data = mDatabase.getBills();
@@ -36,7 +47,7 @@ public class Events extends Fragment{
                 //list.add(data.getString(0));
                 list.add(data.getString(1));
                 list.add(data.getString(2));
-                ArrayAdapter<String> mAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, list);
+                ArrayAdapter<String> mAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, list);
                 listView.setAdapter(mAdapter);
             }
         }
