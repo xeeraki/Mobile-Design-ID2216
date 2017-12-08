ff7ad5c27a9915de5c2e0e2161be2e52e6135a4f
Cassius Garat <cassius.garat@phelma.grenoble-inp.fr>
2017-12-08 17:12:03 +0100

    EventPage: better dynamic layout

--- androidApp/BillPie3/app/src/main/java/com/mobiledesigngroup/billpie3/EventPage.java
+++ androidApp/BillPie3/app/src/main/java/com/mobiledesigngroup/billpie3/EventPage.java
@@ -2,19 +2,28 @@ package com.mobiledesigngroup.billpie3;
 
 import android.database.Cursor;
 import android.database.MatrixCursor;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.ListFragment;
 import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.ScrollView;
+import android.widget.TableLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.graphics.Typeface;
@@ -40,18 +49,28 @@ public class EventPage extends AppCompatActivity {
 
     private DatabaseReference mDatabase;
     private String eventId = "event1";
+    private ProgressBar progBar;
+    private ScrollView scroll;
+    private LinearLayout linearEventPage;
+    private ArrayList<String> spendingTitle;
+    private ArrayList<String> spendingAmount;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.event_page);
-
-        final ListView listView = findViewById(R.id.eventPageListView);
+        setContentView(R.layout.eventpage_dyna);
+        this.progBar = findViewById(R.id.indeterminateBarPay);
+        this.scroll = findViewById(R.id.scroll_card_pay);
+        this.linearEventPage = findViewById(R.id.linear_eventpage);
+        //final ListView listView = findViewById(R.id.eventPageListView);
 
         mDatabase =  FirebaseDatabase.getInstance().getReference();
 
-        final ArrayList<String> spendingTitle = new ArrayList<>();
-        final ArrayList<String> spendingAmount = new ArrayList<>();
+        progBar.setVisibility(View.VISIBLE);
+        scroll.setVisibility(View.INVISIBLE);
+
+        spendingTitle = new ArrayList<>();
+        spendingAmount = new ArrayList<>();
 
         DatabaseReference spendingReference = mDatabase.child("events").child(eventId).child("spendings");
 
@@ -65,7 +84,7 @@ public class EventPage extends AppCompatActivity {
                     spendingTitle.add(retrievedTitle);
                     spendingAmount.add(retrievedAmount);
 
-                    ArrayAdapter adapter = new ArrayAdapter(EventPage.this, android.R.layout.simple_list_item_2,
+                    /*ArrayAdapter adapter = new ArrayAdapter(EventPage.this, android.R.layout.simple_list_item_2,
                             android.R.id.text1, spendingAmount) {
                         @Override
                         public View getView(int position, View convertView, ViewGroup parent) {
@@ -79,8 +98,11 @@ public class EventPage extends AppCompatActivity {
                             return view;
                         }
                     };
-                    listView.setAdapter(adapter);
+                    listView.setAdapter(adapter);*/
                 }
+                progBar.setVisibility(View.INVISIBLE);
+                scroll.setVisibility(View.VISIBLE);
+                displayDyna();
             }
             @Override
             public void onCancelled(DatabaseError databaseError) {
@@ -89,5 +111,140 @@ public class EventPage extends AppCompatActivity {
         };
 
         spendingReference.addValueEventListener(spendingListener);
+    }
+
+    private void displayDyna(){
+        CardView cardView = createNewCardView();
+        View view = createBlueRectangleView();
+        cardView.addView(view);
+        LinearLayout linearFirst = createVerticalLinearLayout();
+        linearFirst.addView(createCardViewTitle("Spendings"));
+
+        int index = 0;
+        while (index < spendingTitle.size()) {
+            LinearLayout paybackLayout = createHorizontalLinearLayout();
+            paybackLayout.addView(createTextStart(spendingTitle.get(index)));
+            paybackLayout.addView(createTextEnd(spendingAmount.get(index)));
+            linearFirst.addView(paybackLayout);
+            index++;
+        }
+        cardView.addView(linearFirst);
+        this.linearEventPage.addView(cardView);
+    }
+
+    private TextView createCardViewTitle(String text) {
+        TextView toPayText = new TextView(EventPage.this);
+
+        toPayText.setText(text);
+        toPayText.setTextColor(Color.BLACK);
+        toPayText.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18);
+        toPayText.setTypeface(Typeface.create("@font/roboto", Typeface.BOLD));
+        TableLayout.LayoutParams toPayTextParams = new TableLayout.LayoutParams(
+                TableLayout.LayoutParams.WRAP_CONTENT,
+                TableLayout.LayoutParams.WRAP_CONTENT,
+                1.0f
+        );
+        toPayTextParams.setMargins(dpToPixel(22), 0, 0, dpToPixel(8));
+        toPayText.setLayoutParams(toPayTextParams);
+
+        return toPayText;
+    }
+
+    private TextView createTextStart(String text) {
+        TextView paybackNameUser = new TextView(EventPage.this);
+
+        paybackNameUser.setText(text);
+        paybackNameUser.setTextColor(Color.BLACK);
+        paybackNameUser.setGravity(Gravity.START);
+        paybackNameUser.setTextSize(TypedValue.COMPLEX_UNIT_SP, 15);
+        paybackNameUser.setTypeface(Typeface.create("@font/roboto", Typeface.NORMAL));
+        TableLayout.LayoutParams paybackNameUserParams = new TableLayout.LayoutParams(
+                TableLayout.LayoutParams.WRAP_CONTENT,
+                TableLayout.LayoutParams.WRAP_CONTENT,
+                1.0f
+        );
+        paybackNameUserParams.setMargins(dpToPixel(22), 0, 0, 0);
+        paybackNameUser.setLayoutParams(paybackNameUserParams);
+
+        return paybackNameUser;
+    }
+
+    private TextView createTextEnd(String text) {
+        TextView paybackAmount = new TextView(EventPage.this);
+
+        paybackAmount.setText(text);
+        paybackAmount.setTextColor(Color.BLACK);
+        paybackAmount.setGravity(Gravity.END);
+        paybackAmount.setTextSize(TypedValue.COMPLEX_UNIT_SP, 15);
+        paybackAmount.setTypeface(Typeface.create("@font/roboto", Typeface.NORMAL));
+        TableLayout.LayoutParams paybackAmountParams = new TableLayout.LayoutParams(
+                TableLayout.LayoutParams.WRAP_CONTENT,
+                TableLayout.LayoutParams.WRAP_CONTENT,
+                1.0f
+        );
+        paybackAmountParams.setMargins(dpToPixel(22), 0, 0, 0);
+        paybackAmount.setLayoutParams(paybackAmountParams);
+
+        return paybackAmount;
+    }
+
+    private CardView createNewCardView() {
+        CardView cardView = new CardView(EventPage.this);
+
+        CardView.LayoutParams cardParams = new CardView.LayoutParams(
+                CardView.LayoutParams.MATCH_PARENT,
+                CardView.LayoutParams.WRAP_CONTENT
+        );
+        cardParams.setMargins(dpToPixel(2) ,0,dpToPixel(2),dpToPixel(8));
+        cardView.setCardBackgroundColor(Color.WHITE);
+        cardView.setLayoutParams(cardParams);
+        cardView.setRadius(dpToPixel(2));
+
+        return cardView;
+    }
+
+    private LinearLayout createHorizontalLinearLayout() {
+        LinearLayout paybackLayout = new LinearLayout(EventPage.this);
+
+        LinearLayout.LayoutParams pbLayoutParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        paybackLayout.setLayoutParams(pbLayoutParams);
+        paybackLayout.setOrientation(LinearLayout.HORIZONTAL);
+
+        return paybackLayout;
+    }
+
+    private LinearLayout createVerticalLinearLayout() {
+        LinearLayout linearFirst = new LinearLayout(EventPage.this);
+
+        LinearLayout.LayoutParams linearFirstParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        linearFirst.setPadding(dpToPixel(16), dpToPixel(16), dpToPixel(16), dpToPixel(16));
+        linearFirst.setLayoutParams(linearFirstParams);
+        linearFirst.setOrientation(LinearLayout.VERTICAL);
+        return linearFirst;
+    }
+
+    private View createBlueRectangleView() {
+        View view = new View(EventPage.this);
+
+        ViewGroup.LayoutParams viewParams = new ViewGroup.LayoutParams(
+                dpToPixel(20),
+                ViewGroup.LayoutParams.MATCH_PARENT
+        );
+        view.setBackground(getResources().getDrawable(R.drawable.rectangle_indigo));
+        view.setLayoutParams(viewParams);
+
+        return view;
+    }
+
+    private int dpToPixel(float dp) {
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
+        float fpixels = metrics.density * dp;
+        return (int) (fpixels + 0.5f);
     }
 }
